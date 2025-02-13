a91bd48794f8527743037d600dae4bf8f8787715
wahidman <wahidman948@gmail.com>
2025-02-13 22:29:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const midtransClient = require('midtrans-client');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
