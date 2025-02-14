46cae0b8dc41501768e209205bd9c4d2aba19a00
wahidman <wahidman948@gmail.com>
2025-02-14 16:22:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ try {
 // Koneksi ke PostgreSQL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Untuk menghindari error SSL
-        }
-    }
+    //dialectOptions: {
+      //  ssl: {
+        //    require: true,
+          //  rejectUnauthorized: false // Untuk menghindari error SSL
+        //}
+   // }
 });
 
 
